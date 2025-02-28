1b0f3a7996631f7ca9b2fba6bc14ca519606dc6f
Coderx85 <work.priyanshu085@gmail.com>
2025-02-28 13:57:45 +0530

    add user actions, and implement industry insights generation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,59 +5,69 @@ import { UserButton } from '@clerk/nextjs'
 import { Book, Headphones, Search } from 'lucide-react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { auth } from "@clerk/nextjs/server";
 
-type Props = {}
-
-const Navbar = (props: Props) => {
+const Navbar = async () => {
+  const { userId } = await auth();
   return (
-    <div className='flex container w-full mx-auto bg-black/70 p-2 pb-6 border-b-4 border-gray-800'>  
-      <div className='flex justify-start'>
-        <Link href='/'>
-          <Image src="/logo.png" alt="logo" width={200} height={50} />
-        </Link>
+      <div className='flex w-full container mx-auto my-0 gap-10 items-center bg-black/50 py-4 border-b-4 border-primary rounded-xl'>  
+        <div className='flex justify-start'>
+          <Link href='/'>
+            <Image src="/logo.png" alt="logo" width={200} height={50} />
+          </Link>
+        </div>
+
+        <div className='flex  gap-8 text-white justify-center'>
+            <Link href={'/dashboard'}>Dashboard</Link>
+            <Link href={'/onboarding'}>Onboarding</Link>
+            <Link href={'/interview'}>Interview</Link>
+            <Link href={'/cover-letter'}>Cover Letter</Link>
+            <Link href={'/reports'}>Reports</Link>
+        </div>
+
+        <div className='flex float-right gap-8 mx-auto p-4 text-2xl text-white'>
+          {/* <span className="flex items-center gap-2 font-bold">
+            <p className="text-sm font-light text-gray-300">Credits</p>
+            <span>0</span>  
+          </span> */}
+          <span className="flex items-center rounded-md bg-muted px-4">
+            <Search />  
+            <Input
+              placeholder="Quick Search"
+              className="border-none bg-transparent"
+            />
+          </span>
+          <TooltipProvider>
+            <Tooltip delayDuration={800}>
+              <TooltipTrigger>
+                <Link href='/contact-us'>
+                  <Headphones />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Contact Support</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider>
+            <Tooltip delayDuration={800}>
+              <TooltipTrigger>
+                <Book />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Guide</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          {userId ?
+            <UserButton />
+          : (
+            <Link href='/sign-in'>
+              Sign In
+            </Link>
+          )}
+        </div>  
       </div>
-
-      <div>
-
-      </div>
-
-      <div className='flex float-end gap-8 mx-auto p-4 text-2xl text-white'>
-        {/* <span className="flex items-center gap-2 font-bold">
-          <p className="text-sm font-light text-gray-300">Credits</p>
-          <span>0</span>
-        </span> */}
-        <span className="flex items-center rounded-md bg-muted px-4">
-          <Search />  
-          <Input
-            placeholder="Quick Search"
-            className="border-none bg-transparent"
-          />
-        </span>
-        <TooltipProvider>
-          <Tooltip delayDuration={0}>
-            <TooltipTrigger>
-              <Link href='/contact-us'>
-                <Headphones />
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p>Contact Support</p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <TooltipProvider>
-          <Tooltip delayDuration={0}>
-            <TooltipTrigger>
-              <Book />
-            </TooltipTrigger>
-            <TooltipContent>
-              <p>Guide</p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <UserButton />
-      </div>  
-    </div>
   )
 }
 
