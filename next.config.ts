6200e652e7c19335f77b078f900a28bfd107d038
Coderx85 <work.priyanshu085@gmail.com>
2025-03-02 07:12:49 +0530

    complete the project.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images:{
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me"
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com"
+      },
+      {
+        protocol: "https",
+        hostname: "assets.aceternity.com"
+      }
+    ]
+  }
   /* config options here */
 };
 
